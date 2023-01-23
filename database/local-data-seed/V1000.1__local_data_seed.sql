923da86c9785722b93145172fa0cf72ddf7cb3fd
Campbell Gilpin <Campbell.Gilpin@arrivia.com>
2023-01-23 18:02:37 +0000

    filling out seed data

--- database/local-data-seed/V1000.1__local_data_seed.sql
+++ database/local-data-seed/V1000.1__local_data_seed.sql
@@ -1,13 +1,20 @@
-insert into developers (name) values ('Naughty Dog');
-insert into developers (name) values ('Santa Monica Studios');
-insert into developers (name) values ('FromSoftware');
-insert into developers (name) values ('Firaxis Games');
-insert into developers (name) values ('PlatinumGames Inc.');
+insert into developers ([name]) values ('Naughty Dog');
+insert into developers ([name]) values ('Santa Monica Studios');
+insert into developers ([name]) values ('FromSoftware');
 
-insert into platforms (name) values ('Playstation 4');
-insert into platforms (name) values ('Playstation 5');
-insert into platforms (name) values ('Nintendo Switch');
-insert into platforms (name) values ('Microsoft Windows');
+insert into games ([name], [description], [developer_id]) values ('God of War Ragnarok', 'Embark on an epic and heartfelt journey as Kratos and Atreus struggle with holding on and letting go.' , (select id from developers where [name] ='Santa Monica Studios'))
+insert into games ([name], [description], [developer_id]) values ('Uncharted 4: A Thief''s End', 'Several years after his last adventure, retired fortune hunter Nathan Drake, is forced back into the world of thieves.' , (select id from developers where [name] ='Naughty Dog'))
+insert into games ([name], [description], [developer_id]) values ('Elden Ring', 'In the Lands Between ruled by Queen Marika the Eternal, the Elden Ring, the source of the Erdtree, has been shattered.' , (select id from developers where [name] ='Naughty Dog'))
+
+insert into platforms ([name]) values ('Playstation 4');
+insert into platforms ([name]) values ('Playstation 5');
+insert into platforms ([name]) values ('Microsoft Windows');
 
 insert into users ([first_name], [last_name], [email], [password]) values ('Campbell', 'Gilpin', 'campbell.gilpin@unosquare.com', 'Password123');
 
+insert into users games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War Ragnarok'), (select id from platforms where [name] ='Playstation 5'), '2022-11-09')
+insert into users games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War'), (select id from platforms where [name] ='Playstation 4'), '2018-04-20')
+insert into users games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War'), (select id from platforms where [name] ='Microsoft Windows'), '2022-01-14')
+insert into users games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Elden Ring'), (select id from platforms where [name] ='Playstation 4'), '2022-02-25')
+insert into users games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Elden Ring'), (select id from platforms where [name] ='Playstation 5'), '2022-02-25')
+insert into users games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Elden Ring'), (select id from platforms where [name] ='Microsoft Windows'), '2022-02-25')
