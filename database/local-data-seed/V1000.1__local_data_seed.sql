7cdbe1c1a4d788cc87c62220013a2526ef768d86
Campbell Gilpin <Campbell.Gilpin@arrivia.com>
2023-01-23 17:32:51 +0000

    filling out seed data

---
+++ database/local-data-seed/V1000.1__local_data_seed.sql
@@ -0,0 +1,13 @@
+insert into developers (name) values ('Naughty Dog');
+insert into developers (name) values ('Santa Monica Studios');
+insert into developers (name) values ('FromSoftware');
+insert into developers (name) values ('Firaxis Games');
+insert into developers (name) values ('PlatinumGames Inc.');
+
+insert into platforms (name) values ('Playstation 4');
+insert into platforms (name) values ('Playstation 5');
+insert into platforms (name) values ('Nintendo Switch');
+insert into platforms (name) values ('Microsoft Windows');
+
+insert into users ([first_name], [last_name], [email], [password]) values ('Campbell', 'Gilpin', 'campbell.gilpin@unosquare.com', 'Password123');
+
