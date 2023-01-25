72c6fda1a73ecdf2161f34a3182b52cf89139b0e
Campbell Gilpin <Campbell.Gilpin@arrivia.com>
2023-01-25 14:38:53 +0000

    Updated SQL and added API project

--- database/local-data-seed/V1000.1__local_data_seed.sql
+++ database/local-data-seed/V1000.1__local_data_seed.sql
@@ -1,33 +1,38 @@
-insert into developers ([name]) values ('Naughty Dog');
-insert into developers ([name]) values ('Santa Monica Studios');
-insert into developers ([name]) values ('FromSoftware');
+insert into developers (name) values ('Naughty Dog');
+insert into developers (name) values ('Santa Monica Studios');
+insert into developers (name) values ('FromSoftware');
 
-insert into games ([name], [description], [developer_id]) values ('God of War', 'His vengeance against the Gods of Olympus years behind him, Kratos now lives as a man in the realm of Norse Gods and monsters.' , (select id from developers where [name] ='Santa Monica Studios'))
-insert into games ([name], [description], [developer_id]) values ('God of War Ragnarok', 'Embark on an epic and heartfelt journey as Kratos and Atreus struggle with holding on and letting go.' , (select id from developers where [name] ='Santa Monica Studios'))
-insert into games ([name], [description], [developer_id]) values ('Uncharted 4', 'Several years after his last adventure, retired fortune hunter Nathan Drake, is forced back into the world of thieves.' , (select id from developers where [name] ='Naughty Dog'))
-insert into games ([name], [description], [developer_id]) values ('Elden Ring', 'In the Lands Between ruled by Queen Marika the Eternal, the Elden Ring, the source of the Erdtree, has been shattered.' , (select id from developers where [name] ='Naughty Dog'))
+insert into games (name, description, developer_id) values ('God of War', 'His vengeance against the Gods of Olympus years behind him, Kratos now lives as a man in the realm of Norse Gods and monsters.' , (select id from developers where name ='Santa Monica Studios'));
+insert into games (name, description, developer_id) values ('God of War Ragnarok', 'Embark on an epic and heartfelt journey as Kratos and Atreus struggle with holding on and letting go.' , (select id from developers where name ='Santa Monica Studios'));
+insert into games (name, description, developer_id) values ('Uncharted 4', 'Several years after his last adventure, retired fortune hunter Nathan Drake, is forced back into the world of thieves.' , (select id from developers where name ='Naughty Dog'));
+insert into games (name, description, developer_id) values ('Elden Ring', 'In the Lands Between ruled by Queen Marika the Eternal, the Elden Ring, the source of the Erdtree, has been shattered.' , (select id from developers where name ='FromSoftware'));
 
-insert into platforms ([name]) values ('Playstation 4');
-insert into platforms ([name]) values ('Playstation 5');
-insert into platforms ([name]) values ('Microsoft Windows');
+insert into platforms (name) values ('Playstation 4');
+insert into platforms (name) values ('Playstation 5');
+insert into platforms (name) values ('Microsoft Windows');
 
-insert into users ([first_name], [last_name], [email], [password]) values ('Campbell', 'Gilpin', 'campbell.gilpin@unosquare.com', 'Password123');
+insert into users (first_name, last_name, email, password) values ('Campbell', 'Gilpin', 'campbell.gilpin@unosquare.com', 'Password123');
 
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War Ragnarok'), (select id from platforms where [name] ='Playstation 5'), '2022-11-09')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War Ragnarok'), (select id from platforms where [name] ='Playstation 4'), '2022-11-09')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War'), (select id from platforms where [name] ='Playstation 4'), '2018-04-20')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='God of War'), (select id from platforms where [name] ='Microsoft Windows'), '2022-01-14')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Elden Ring'), (select id from platforms where [name] ='Playstation 4'), '2022-02-25')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Elden Ring'), (select id from platforms where [name] ='Playstation 5'), '2022-02-25')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Elden Ring'), (select id from platforms where [name] ='Microsoft Windows'), '2022-02-25')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Uncharted 4'), (select id from platforms where [name] ='Playstation 4'), '2016-05-10')
-insert into games_platforms([game_id], [platform_id], [release_date]) values ((select id from games where [name] ='Uncharted 4'), (select id from platforms where [name] ='Microsoft Windows'), '2022-10-19')
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='God of War Ragnarok'), (select id from platforms where name ='Playstation 5'), '2022-11-09');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='God of War Ragnarok'), (select id from platforms where name ='Playstation 4'), '2022-11-09');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='God of War'), (select id from platforms where name ='Playstation 4'), '2018-04-20');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='God of War'), (select id from platforms where name ='Microsoft Windows'), '2022-01-14');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='Elden Ring'), (select id from platforms where name ='Playstation 4'), '2022-02-25');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='Elden Ring'), (select id from platforms where name ='Playstation 5'), '2022-02-25');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='Elden Ring'), (select id from platforms where name ='Microsoft Windows'), '2022-02-25');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='Uncharted 4'), (select id from platforms where name ='Playstation 4'), '2016-05-10');
+insert into games_platforms(game_id, platform_id, release_date) values ((select id from games where name ='Uncharted 4'), (select id from platforms where name ='Microsoft Windows'), '2022-10-19');
 
-insert into users_games ([user_id], [game_platform_id]) values ((select id from users where [first_name] ='Campbell'), (select id from games_platforms where [game_id] = (select id from games where [name] ='God of War Ragnarok') AND [platform_id] = (select id from platforms where [name] ='Playstation 5')))
-insert into users_games ([user_id], [game_platform_id]) values ((select id from users where [first_name] ='Campbell'), (select id from games_platforms where [game_id] = (select id from games where [name] ='God of War') AND [platform_id] = (select id from platforms where [name] ='Playstation 4')))
-insert into users_games ([user_id], [game_platform_id]) values ((select id from users where [first_name] ='Campbell'), (select id from games_platforms where [game_id] = (select id from games where [name] ='Elden Ring') AND [platform_id] = (select id from platforms where [name] ='Playstation 5')))
-insert into users_games ([user_id], [game_platform_id]) values ((select id from users where [first_name] ='Campbell'), (select id from games_platforms where [game_id] = (select id from games where [name] ='Uncharted 4') AND [platform_id] = (select id from platforms where [name] ='Playstation 4')))
+insert into users_games (user_id, game_platform_id) values ((select id from users where first_name ='Campbell'), (select id from games_platforms where game_id = (select id from games where name ='God of War Ragnarok') AND platform_id = (select id from platforms where name ='Playstation 5')));
+insert into users_games (user_id, game_platform_id) values ((select id from users where first_name ='Campbell'), (select id from games_platforms where game_id = (select id from games where name ='God of War') AND platform_id = (select id from platforms where name ='Playstation 4')));
+insert into users_games (user_id, game_platform_id) values ((select id from users where first_name ='Campbell'), (select id from games_platforms where game_id = (select id from games where name ='Elden Ring') AND platform_id = (select id from platforms where name ='Playstation 5')));
+insert into users_games (user_id, game_platform_id) values ((select id from users where first_name ='Campbell'), (select id from games_platforms where game_id = (select id from games where name ='Uncharted 4') AND platform_id = (select id from platforms where name ='Playstation 4')));
 
-insert into completions ([datetime], [user_game_id]) values ('2023-01-02', (select id from users_games where [user_id] = (select id from users where [first_name] ='Campbell') AND [game_platform_id] = (select id from games_platforms where [game_id] = (select id from games where [name] ='God of War Ragnarok') AND [platform_id] = (select id from platforms where [name] ='Playstation 5'))))
+insert into reviews (star_rating, description, date_reviewed, game_id, user_id) values (5, 'Good stuff!', '2023-01-23', (select id from games where name ='God of War Ragnarok'), (select id from users where first_name ='Campbell'));
 
-insert into reviews ([star_rating], [description], [date_reviewed], [game_id], [user_id]) values (5, 'Good stuff!', '2023-01-23', (select id from games where [name] ='God of War Ragnarok'), (select id from users where [first_name] ='Campbell'))
+insert into date_types (date_type) values ('Purchase Date');
+insert into date_types (date_type) values ('Completed Date');
+
+insert into game_dates (date, date_type_id, user_game_id) values ('2022-12-11', (select id from date_types where date_type ='Purchase Date'), (select id from users_games where user_id = (select id from users where first_name ='Campbell') AND game_platform_id = (select id from games_platforms where game_id = (select id from games where name ='God of War Ragnarok') AND platform_id = (select id from platforms where name ='Playstation 5'))));
+insert into game_dates (date, date_type_id, user_game_id) values ('2023-01-02', (select id from date_types where date_type ='Completed Date'), (select id from users_games where user_id = (select id from users where first_name ='Campbell') AND game_platform_id = (select id from games_platforms where game_id = (select id from games where name ='God of War Ragnarok') AND platform_id = (select id from platforms where name ='Playstation 5'))));
+
--- database/migrations/V1.0__initial_schema_setup.sql
+++ database/migrations/V1.0__initial_schema_setup.sql
@@ -1,51 +1,63 @@
 CREATE TABLE public.users (
-    [id] SERIAL NOT NULL,
-    [first_name] VARCHAR(255) NOT NULL,
-    [last_name] VARCHAR(255) NOT NULL,
-    [email] VARCHAR(255) NOT NULL,
-    [password] VARCHAR(255) NOT NULL,
+    id SERIAL NOT NULL,
+    first_name VARCHAR(255) NOT NULL,
+    last_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
     CONSTRAINT users_pkey PRIMARY KEY (id)
 );
-CREATE TABLE public.users_games (
-    [id] SERIAL NOT NULL,
-    [user_id] SERIAL NOT NULL,
-    [game_platform_id] SERIAL NOT NULL
-);
-CREATE TABLE public.reviews (
-    [id] SERIAL NOT NULL,
-    [star_rating] INT NOT NULL,
-    [description] VARCHAR(255) NOT NULL,
-    [date_reviewed] DATE NOT NULL,
-    [game_id] INT NOT NULL CONSTRAINT reviews_game_platforms_game_platform_id_fk REFERENCES public.games_platforms,
-    [user_id] INT NOT NULL CONSTRAINT reviews_users_user_id_fk REFERENCES public.users,
-    CONSTRAINT reviews_pkey PRIMARY KEY (id)
-);
-CREATE TABLE public.completions (
-    [id] SERIAL NOT NULL,
-    [datetime] DATE NOT NULL,
-    [user_game_id] INT NOT NULL CONSTRAINT completions_users_games_user_game_id_fk REFERENCES public.users_games,
-    CONSTRAINT completions_pkey PRIMARY KEY (id)
-);
 CREATE TABLE public.platforms (
-    [id] SERIAL NOT NULL,
-    [name] VARCHAR(255),
+    id SERIAL NOT NULL,
+    name VARCHAR(255),
     CONSTRAINT platforms_pkey PRIMARY KEY (id)
 );
 CREATE TABLE public.developers (
-    [id] SERIAL NOT NULL,
-    [name] VARCHAR(255),
+    id SERIAL NOT NULL,
+    name VARCHAR(255),
     CONSTRAINT developers_pkey PRIMARY KEY (id)
 );
 CREATE TABLE public.games (
-    [id] SERIAL NOT NULL,
-    [name] VARCHAR(255),
-    [description] VARCHAR(255),
-    [developer_id] INT NOT NULL CONSTRAINT games_reviews_developer_id_fk REFERENCES public.developers,
+    id SERIAL NOT NULL,
+    name VARCHAR(255),
+    description VARCHAR(255),
+    developer_id INT NOT NULL CONSTRAINT games_reviews_developer_id_fk REFERENCES public.developers,
     CONSTRAINT games_pkey PRIMARY KEY (id)
 );
 CREATE TABLE public.games_platforms (
-    [id] SERIAL NOT NULL,
-    [game_id] INT NOT NULL CONSTRAINT games_platforms_users_games_game_id_fk REFERENCES public.users_games,
-    [platform_id] INT NOT NULL CONSTRAINT completions_platforms_platform_id_fk REFERENCES public.platforms,
-    [release_date] DATE NOT NULL
+    id SERIAL NOT NULL,
+    game_id INT NOT NULL CONSTRAINT games_platforms_games_game_id_fk REFERENCES public.games,
+    platform_id INT NOT NULL CONSTRAINT completions_platforms_platform_id_fk REFERENCES public.platforms,
+    release_date DATE NOT NULL,
+    CONSTRAINT games_platforms_pkey PRIMARY KEY (id)
 );
+CREATE TABLE public.users_games (
+    id SERIAL NOT NULL,
+    user_id INT NOT NULL,
+    game_platform_id INT NOT NULL CONSTRAINT users_games_games_platforms_game_platform_id_fk REFERENCES public.games_platforms,
+    CONSTRAINT users_games_pkey PRIMARY KEY (id)
+);
+CREATE TABLE public.reviews (
+    id SERIAL NOT NULL,
+    star_rating INT NOT NULL,
+    description VARCHAR(255) NOT NULL,
+    date_reviewed DATE NOT NULL,
+    game_id INT NOT NULL CONSTRAINT reviews_game_platforms_game_platform_id_fk REFERENCES public.games_platforms,
+    user_id INT NOT NULL CONSTRAINT reviews_users_user_id_fk REFERENCES public.users,
+    CONSTRAINT reviews_pkey PRIMARY KEY (id)
+);
+CREATE TABLE public.date_types (
+    id SERIAL NOT NULL,
+    date_type VARCHAR(255),
+    CONSTRAINT completions_pkey PRIMARY KEY (id)
+);
+CREATE TABLE public.game_dates (
+    id SERIAL NOT NULL,
+    date DATE NOT NULL,
+    date_type_id INT NOT NULL CONSTRAINT game_dates_date_types_date_type_id_fk REFERENCES public.date_types, 
+    user_game_id INT NOT NULL CONSTRAINT game_dates_users_games_user_game_id_fk REFERENCES public.users_games,
+    CONSTRAINT game_dates_pkey PRIMARY KEY (id)
+);
+
+
+
+
